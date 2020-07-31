4a931823e8ca031e972902a71a854a7f4c7344b7
Morgan <mel73@duke.edu>
2020-07-30 21:45:21 -0400

    navigate between screens and clean up files

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Dimensions,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  SafeAreaView,
+  Button,
+  Platform,
+} from "react-native";
 
-export default function App() {
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import HomeScreen from "./app/screens/HomeScreen";
+import MapScreen from "./app/screens/MapScreen";
+import HealthScreen from "./app/screens/HealthScreen";
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Morgan's Personal Project</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Maps" component={MapScreen} />
+        <Stack.Screen name="Health" component={HealthScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
+
+// flexDirection: row or column
+// justifyContent: center...
+// alignItems: center...
